7269465b1b77e92b7270941bc9f98b3a5a1b2c14
Gabriel Puga <g_puga@hotmail.com>
2024-05-22 09:08:53 -0600

    commiting modification to file 02.JoinView.sql

--- 02.JoinView.sql
+++ 02.JoinView.sql
@@ -1,8 +1,8 @@
 use littlelemondb;
-CREATE VIEW orders_over_fifty AS
+CREATE VIEW over_fifteen_hundred AS
 SELECT c.customer_id, c.customer_name, o.order_id, o.total_cost, m.item_name, mt.item_type
 FROM customers AS c
 INNER JOIN orders AS o ON c.customer_id = o.customer_id
 INNER JOIN menu AS m ON o.menu_item_id = m.menu_item_id
 INNER JOIN menutypes AS mt ON m.item_type_id = mt.item_type_id
-WHERE o.total_cost > 50;
+WHERE o.total_cost > 150;
